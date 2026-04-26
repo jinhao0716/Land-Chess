fcd5ee64f18d1cd4fd4ecec0b7a5841b66d72aed
Jin Hao Fan <jhfan0716@gmail.com>
2026-04-25 23:58:33 -0600

    Fixed not being able to see Gridpane child objects

--- src/main/ui/controllers/BoardController.java
+++ src/main/ui/controllers/BoardController.java
@@ -1,23 +1,24 @@
 package main.ui.controllers;
 
-import javafx.scene.Node;
+import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 import javafx.scene.layout.GridPane;
 import javafx.scene.control.Button;
 
 public class BoardController {
+    @FXML
     public GridPane upperBoard;
+    @FXML
     public GridPane lowerBoard;
 
-    Label test = new Label("连长");
-
     public void initialize(){
-        upperBoard = new GridPane();
-        lowerBoard = new GridPane();
-        lowerBoard.add(test, 0, 0);
-        test.setVisible(true);
-        lowerBoard.setVisible(true);
-        lowerBoard.setGridLinesVisible(true);
 
+        for(int i = 0; i < 5; i++){
+            for(int j = 0; j < 6; j++){
+                Button test = new Button("连长");
+                test.setPrefSize(100, 30);
+                lowerBoard.add(test, i, j);
+            }
+        }
     }
 }
