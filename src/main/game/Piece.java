7142bf2525cbc691c3529aff08b6fb89dd811bc8
Jin Hao Fan <jhfan0716@gmail.com>
2026-04-26 18:59:19 -0600

    Added labels for pieces. Created getLabel and getRank functions

--- src/main/game/Piece.java
+++ src/main/game/Piece.java
@@ -9,53 +9,74 @@ public class Piece {
     private boolean mine = false;
     private boolean bomb = false;
     private int rankVal;
+    private String label = "";
 
     public Piece(Character rank){
         this.rank = rank;
         switch(rank){
             case '1':
                 this.rankVal = 1;
+                this.label = "司令";
                 break;
             case '2':
                 this.rankVal = 2;
+                this.label = "军长";
                 break;
             case '3':
                 this.rankVal = 3;
+                this.label = "师长";
                 break;
             case '4':
                 this.rankVal = 4;
+                this.label = "旅长";
                 break;
             case '5':
                 this.rankVal = 5;
+                this.label = "团长";
                 break;
             case '6':
                 this.rankVal = 6;
+                this.label = "营长";
                 break;
             case '7':
                 this.rankVal = 7;
+                this.label = "连长";
                 break;
             case '8':
                 this.rankVal = 8;
+                this.label = "排长";
                 break;
             case '9':
                 this.rankVal = 9;
                 this.canTurn = true;
                 this.canMine = true;
+                this.label = "工兵";
                 break;
             case 'M':
                 this.rankVal = -1;
                 this.mine = true;
                 this.canMove = false;
+                this.label = "地雷";
                 break;
             case 'B':
-                this.rankVal = -1;
+                this.rankVal = -2;
                 this.bomb = true;
+                this.label = "炸弹";
                 break;
             case 'F':
                 this.rankVal = 999;
                 this.winCondition = true;
                 this.canMove = false;
+                this.label = "军旗";
                 break;
         }
+    }
+
+    public String getLabel(){
+        return this.label;
+    }
+
+    public int getRank(){
+        return this.rankVal;
     }
 }
